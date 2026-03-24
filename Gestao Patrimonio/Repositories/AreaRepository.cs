fb450a6c3902d519cf0ab19ac2b33ca5cd9334f3
pedroacg80 <pedroacg80@gmail.com>
2026-03-24 10:53:29 -0300

    :construction: Desenvolvimento das regras na repository

---
+++ Gestao Patrimonio/Repositories/AreaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Gestao_Patrimonio.Contexts;
+using Gestao_Patrimonio.Domains;
+using Gestao_Patrimonio.Interfaces;
+
+namespace Gestao_Patrimonio.Repositories
+{
+    public class AreaRepository : IAreaRepository
+    {
+        private readonly GestaoPatrimoniosContext _context;
+
+        public AreaRepository(GestaoPatrimoniosContext context)
+        {
+            _context = context;
+        }
+
+        public List<Area> Listar()
+        {
+            return _context.Area.OrderBy(area => area.NomeArea).ToList(); //OrderBy trazer a listar por ordem alfabetica
+        }
+
+    }
+}
